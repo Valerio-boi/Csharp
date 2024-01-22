6210b5856be9bf0fc82a3a3808b32fbbc6135c2f
Raffaele FIccadenti <r.ficcadenti@exolab.app>
2024-01-22 11:50:08 +0100

    Avanzamento operation di studio

--- ApiWeb/ApiWeb/Controllers/UserController.cs
+++ ApiWeb/ApiWeb/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Database;
+using ApiWeb.Model.Request;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiWeb.Controllers
@@ -7,9 +9,54 @@ namespace ApiWeb.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetData() => Ok("Test");
+        private static FakeDataBase db = new FakeDataBase();
 
+        [HttpGet("all")]
+        public IActionResult GetAllData()
+        {
+            return Ok(db.Users);
+        }
+
+        [HttpGet]
+        public IActionResult GetUser(int id)
+        {
+            User user = db.Users[id];
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser(int id)
+        {
+
+            User user = db.Users[id];
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+
+            return Ok("User eliminato con successo");
+        }
+
+        [HttpPost]
+        public IActionResult addUser(UserRequest user) {
+            Console.WriteLine("Inserisco lo user");
+
+            db.AddUser(new User
+            {
+                UserName = user.UserName,
+                Name = user.Name,
+                Surname = user.Surname,
+                Password = user.Password
+            });
+
+            return Ok("User inserito correttamente");
+        }
 
     }
 }
